4fa73755ea3bd5d20cc267af156fc46aff99d678
olopez03 <lopez24orlando@gmail.com>
2023-11-10 22:18:00 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.set( 'view engine', 'pug'); // set engine
+app.set( 'views', 'views'); // set views
+const routes = require("./routes/feed");
+
+const bodyParser = require("body-parser");
+const http = require("http");
+const path = require('path');
+
+app.use( bodyParser.urlencoded({extended: false}));
+app.use( express.static( path.join(__dirname, 'public')));
+app.use( routes);
+
+const mysql = require('mysql');
+
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+let port = 3001;
+const server = http.createServer(app);
+server.listen( port );
+console.log( `Listening on http://localhost:${port}`);
